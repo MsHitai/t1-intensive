797da21045ab4d6c494945613128d8e51bf0065a
Trushanina Maria <mmalafeeva@yandex.ru>
2024-10-31 20:35:40 +0300

    finish first task

--- src/test/java/com/t1/intensive/IntensiveApplicationTests.java
+++ src/test/java/com/t1/intensive/IntensiveApplicationTests.java
@@ -1,13 +1,22 @@
 package com.t1.intensive;
 
+import com.t1.intensive.controller.AccountController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class IntensiveApplicationTests {
 
+	@Autowired
+	private AccountController accountController;
+
 	@Test
 	void contextLoads() {
+		assertThat(accountController).isNotNull();
 	}
 
 }
