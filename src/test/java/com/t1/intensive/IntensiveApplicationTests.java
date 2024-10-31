d5aef34bf100744b30e40cb6600dcae64dd35466
Trushanina Maria <mmalafeeva@yandex.ru>
2024-10-31 20:51:06 +0300

    fix ci

--- src/test/java/com/t1/intensive/IntensiveApplicationTests.java
+++ src/test/java/com/t1/intensive/IntensiveApplicationTests.java
@@ -4,11 +4,13 @@ import com.t1.intensive.controller.AccountController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IntensiveApplicationTests {
 
 	@Autowired
